7fa84a97f040ce4ea3a1deb5dbd1d357574372fd
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2025-09-12 10:30:44 +0530

    Updated the agent name and agent dir name

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents import Agent
 
 root_agent = Agent(
-    name="greeting-agent",
+    name="greeting_agent",
     # https://ai.google.dev/gemini-api/docs/models
     model="gemini-2.0-flash",
     description="Greeting Agent",
