eb142ce3b2d2476cb9bc6740935e8b046d2978d8
Sourav <59256726+sourav-sdet@users.noreply.github.com>
2025-10-07 08:23:05 +0530

    added runners and updated sessions

--- 5-sessions-and-state/basic_stateful_session.py
+++ 5-sessions-and-state/basic_stateful_session.py
@@ -1,17 +1,17 @@
 import uuid
-from dotenv import load_dotenv
 
-from google.adk.runners import Runner 
+from dotenv import load_dotenv
+from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
 from google.genai import types
 from question_and_answer_agent import question_and_answer_agent
 
 load_dotenv()
 
-# Create a new session service to store state
-stateful_session_service = InMemorySessionService
 
-# Store the initial state as a dictionary
+# Create a new session service to store state
+session_service_stateful = InMemorySessionService()
+
 initial_state = {
     "user_name": "Sourav Mishra",
     "user_preferences": """
@@ -22,17 +22,44 @@ initial_state = {
     """,
 }
 
-# Create a new session
+# Create a NEW session
 APP_NAME = "Sourav Bot"
-USER_ID = "Sourav"
+USER_ID = "souravmishra436"
 SESSION_ID = str(uuid.uuid4())
-stateful_session = stateful_session_service.create_session(
-    app_name = APP_NAME,
-    user_id = USER_ID,
-    session_id = SESSION_ID,
-    state = initial_state
+stateful_session = session_service_stateful.create_session(
+    app_name=APP_NAME,
+    user_id=USER_ID,
+    session_id=SESSION_ID,
+    state=initial_state,
 )
-
 print("CREATED NEW SESSION:")
 print(f"\tSession ID: {SESSION_ID}")
 
+runner = Runner(
+    agent=question_and_answer_agent,
+    app_name=APP_NAME,
+    session_service=session_service_stateful,
+)
+
+new_message = types.Content(
+    role="user", parts=[types.Part(text="What is Sourav's favorite TV show?")]
+)
+
+for event in runner.run(
+    user_id=USER_ID,
+    session_id=SESSION_ID,
+    new_message=new_message,
+):
+    if event.is_final_response():
+        if event.content and event.content.parts:
+            print(f"Final Response: {event.content.parts[0].text}")
+
+print("==== Session Event Exploration ====")
+session = session_service_stateful.get_session(
+    app_name=APP_NAME, user_id=USER_ID, session_id=SESSION_ID
+)
+
+# Log final Session state
+print("=== Final Session State ===")
+for key, value in session.state.items():
+    print(f"{key}: {value}")
---
+++ 5-sessions-and-state/question_and_answer_agent/__init__.py
@@ -0,0 +1,1 @@
+from .agent import question_and_answer_agent
